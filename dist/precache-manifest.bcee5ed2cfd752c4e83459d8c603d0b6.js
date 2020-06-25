f6b86f6aa86d5e805ac2fd071ba7a12ccad20d0b
Maulana Yusup Abdullah <adeneling@gmail.com>
2020-06-25 11:44:25 +0700

    Upload Test Generate

---
+++ dist/precache-manifest.bcee5ed2cfd752c4e83459d8c603d0b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c40306f87c1e70e50f",
+    "url": "/css/app.0f6937e9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "031232a364c71039ad204af136eb493b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb50c70761b3b59a1495",
+    "url": "/js/about.bd9b7c5e.js"
+  },
+  {
+    "revision": "83c40306f87c1e70e50f",
+    "url": "/js/app.0bf56d78.js"
+  },
+  {
+    "revision": "f9a7078613e7dd666888",
+    "url": "/js/chunk-vendors.0f4403ed.js"
+  },
+  {
+    "revision": "9bb1bfa983e51b91f4e7e782fd858ff8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
